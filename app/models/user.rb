8f8381dd709ef5919c1a66694e5d4652a214d3be
とろゝ <toro-ponz@users.noreply.github.com>
2018-01-25 18:24:55 +0900

    カラム追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name,
+    presence: true,
+    length: { minimum: 1, maximum: 50 }
+
+  validates :screen_name, 
+    presence: true,
+    uniqueness: { case_sensitive: :false }, 
+    length: { minimum: 4, maximum: 20 }, 
+    format: { with: /\A[a-z0-9_]+\z/ } 
 end
--- db/migrate/20180124171033_devise_create_users.rb
+++ db/migrate/20180124171033_devise_create_users.rb
@@ -3,6 +3,10 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
+      t.string :name, null: false
+      t.string :screen_name, null: false
+      t.string :description
+
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -36,6 +40,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       t.timestamps null: false
     end
 
+    add_index :users, :screen_name,          unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
